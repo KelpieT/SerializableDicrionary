fcb5e9c4bf682e5a049d559f2bd6b650db3969ab
Andrey <40003779+KelpieT@users.noreply.github.com>
2023-11-02 16:41:12 +0200

    init

---
+++ Assets/_PackageRoot/Runtime/SerializableDicrionary.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Viter.Dictionary
+{
+    [Serializable]
+    public class SerializableDicrionary<TKey, TValue>
+    {
+        [SerializeField] private SerializablePair<TKey, TValue>[] pairs;
+        private Dictionary<TKey, TValue> dict;
+        public Dictionary<TKey, TValue> Dict
+        {
+            get
+            {
+                if (dict == null)
+                {
+                    dict = new Dictionary<TKey, TValue>();
+                    for (int i = 0; i < pairs.Length; i++)
+                    {
+                        if (dict.ContainsKey(pairs[i].key))
+                        {
+                            continue;
+                        }
+                        dict.Add(pairs[i].key, pairs[i].value);
+                    }
+#if !UNITY_EDITOR
+                    pairs = null;
+#endif
+                }
+                return dict;
+            }
+        }
+
+#if UNITY_EDITOR
+    public void SetPairs_EDITOR_ONLY(TKey[] keys, TValue[] comps)
+    {
+        if (keys.Length != comps.Length)
+        {
+            return;
+        }
+        pairs = new SerializablePair<TKey, TValue>[keys.Length];
+        for (int i = 0; i < keys.Length; i++)
+        {
+            pairs[i] = new SerializablePair<TKey, TValue>() { key = keys[i], value = comps[i] };
+        }
+    }
+#endif
+    }
+
+    [Serializable]
+    public class SerializablePair<TEnum, TComp>
+    {
+        public TEnum key;
+        public TComp value;
+    }
+}
